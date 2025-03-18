d755c3e6e428d12a03aa5cd936ce20668d14536d
Arzoo Mansuri <arzoomansuri45@gmail.com>
2025-03-18 08:59:25 +0530

    Update main.js

--- Color Changing/main.js
+++ Color Changing/main.js
@@ -3,26 +3,6 @@ let btns = document.querySelectorAll('button');
 
 btns.forEach((btn) => {
     btn.addEventListener('click', () => {
-        if(btn.innerText === "Red") {
-        body.style.backgroundColor = "red";
-
-        } else if (btn.innerText === "Orange") {
-        body.style.backgroundColor = "orange";
-
-        } else if (btn.innerText === "Yellow") {
-        body.style.backgroundColor = "yellow"; 
-
-        } else if (btn.innerText === "Green") {
-        body.style.backgroundColor = "green"; 
-          
-        } else if (btn.innerText === "Blue") {
-        body.style.backgroundColor = "blue";
-            
-        } else if (btn.innerText === "Indigo") {
-        body.style.backgroundColor = "indigo";
-            
-        } else if (btn.innerText === "Violet") {
-        body.style.backgroundColor = "violet";  
-        };
+     body.style.backgroundColor = btn.innerText;
     })
 });
